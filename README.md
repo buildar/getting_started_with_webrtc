be19766e40a212909a9c9e5d60814293b1dcc79d
Rob Manson <robman@buildAR.com>
2013-07-22 14:51:05 +1000

    Initial commit of all the code to make the video_call_with_chat_and_file_sharing.html demo application work.

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ getting_started_with_webrtc
 ===========================
 
 Example code to help you get started creating WebRTC applications
+
+For a full description of how all this code works and other examples described in more details see "Getting started with WebRTC" by Rob Manson, published by Packt Publishing (http://www.packtpub.com/).
+
+webrtc_polyfill.js 
+------------------
+(included inline within video_call_with_chat_and_file_sharing.html)
+This is a simple WebRTC polyfill based loosely on adapter.js by  Adam Barth. It is designed to make it easy to write WebRTC code that runs on different browser implementations (e.g. Chrome, Firefox, etc.).
+
+video_call_with_chat_and_file_sharing.html
+------------------------------------------
+This is a basic web page that connects a Caller and a Callee via a Web Socket signaling server to support a video call and text based chat with file sharing.  This utilises the new_file_arriving.png and share_new_file.png files in the images directory.
+
+webrtc_signal_server.js
+-----------------------
+This is a node.js based script that provides Web server and Web Socket server functionality required to support the example html based WebRTC applications.
+
+This requires the "websocket" package that can be installed from the command line by typing "npm install websocket".
+
+To start this server from the command line simply type "node basic_signal_server.js" then point your browser at http://localhost:1234
+You can replace localhost with any ip address you like and you can replace 1234 with any port you like too.
+
+NOTE: It is important that the ip address you use here is also accessible by the other person you want to join your call.
+
---
+++ webrtc_signal_server.js
@@ -0,0 +1,133 @@
+/*
+
+  webrtc_signal_server.js by Rob Manson (buildAR.com)
+
+  The MIT License
+
+  Copyright (c) 2013 Rob Manson, http://buildAR.com. All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+
+*/
+
+// useful libs
+var http = require("http");
+var fs = require("fs");
+var websocket = require("websocket").server;
+
+// general variables
+var port = 1234;
+var webrtc_clients = [];
+var webrtc_discussions = {};
+
+// web server functions
+var http_server = http.createServer(function(request, response) {
+  var matches = undefined;
+  if (matches = request.url.match("^/images/(.*)")) {
+    var path = process.cwd()+"/images/"+matches[1];
+    fs.readFile(path, function(error, data) {
+      if (error) {
+        log_error(error);
+      } else {
+        response.end(data);
+      }
+    });
+  } else {
+    response.end(page);
+  }
+});
+http_server.listen(port, function() {
+  log_comment("server listening (port "+port+")");
+});
+var page = undefined;
+fs.readFile("video_call_with_chat_and_file_sharing.html", function(error, data) {
+  if (error) {
+    log_error(error);
+  } else {
+    page = data;
+  }
+});
+
+// web socket functions
+var websocket_server = new websocket({
+  httpServer: http_server
+});
+websocket_server.on("request", function(request) {
+  log_comment("new request ("+request.origin+")");
+
+  var connection = request.accept(null, request.origin);
+  log_comment("new connection ("+connection.remoteAddress+")");
+
+  webrtc_clients.push(connection);
+  connection.id = webrtc_clients.length-1;
+  
+  connection.on("message", function(message) {
+    if (message.type === "utf8") {
+      log_comment("got message "+message.utf8Data);
+
+      var signal = undefined;
+      try { signal = JSON.parse(message.utf8Data); } catch(e) { };
+      if (signal) {
+        if (signal.type === "join" && signal.token !== undefined) {
+          try {
+            if (webrtc_discussions[signal.token] === undefined) {
+              webrtc_discussions[signal.token] = {};
+            }
+          } catch(e) { };
+          try {
+            webrtc_discussions[signal.token][connection.id] = true;
+          } catch(e) { };
+        } else if (signal.token !== undefined) {
+          try {
+            Object.keys(webrtc_discussions[signal.token]).forEach(function(id) {
+              if (id != connection.id) {
+                webrtc_clients[id].send(message.utf8Data, log_error);
+              }
+            });
+          } catch(e) { };
+        } else {
+          log_comment("invalid signal: "+message.utf8Data);
+        }
+      } else {
+        log_comment("invalid signal: "+message.utf8Data);
+      }
+    }
+  });
+  
+  connection.on("close", function(connection) {
+    log_comment("connection closed ("+connection.remoteAddress+")");    
+    Object.keys(webrtc_discussions).forEach(function(token) {
+      Object.keys(webrtc_discussions[token]).forEach(function(id) {
+        if (id === connection.id) {
+          delete webrtc_discussions[token][id];
+        }
+      });
+    });
+  });
+});
+
+// utility functions
+function log_error(error) {
+  if (error !== "Connection closed" && error !== undefined) {
+    log_comment("ERROR: "+error);
+  }
+}
+function log_comment(comment) {
+  console.log((new Date())+" "+comment);
+}
